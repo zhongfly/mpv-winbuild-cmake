15cc9cd63f196801d4a446547bef76528751a77a
zhongfly <11155705+zhongfly@users.noreply.github.com>
2025-03-09 19:30:10 +0800

    packages: add python3-embed

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -16,6 +16,7 @@ ExternalProject_Add(mpv
         uchardet
         openal-soft
         mujs
+        python3-embed
         vulkan
         shaderc
         libplacebo
@@ -24,8 +25,11 @@ ExternalProject_Add(mpv
         libsdl2
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
+    GIT_REMOTE_NAME origin
+    GIT_TAG master
+    GIT_CLONE_FLAGS ""
     UPDATE_COMMAND ""
+    PATCH_COMMAND git fetch origin pull/11574/head:pr && git pull -f && git merge --squash pr
     CONFIGURE_COMMAND ${EXEC} CONF=1 meson setup <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
         --libdir=${MINGW_INSTALL_PREFIX}/lib
@@ -41,6 +45,7 @@ ExternalProject_Add(mpv
         -Dpdf-build=enabled
         -Dlua=enabled
         -Djavascript=enabled
+        -Dpython=enabled
         -Dsdl2=enabled
         -Dlibarchive=enabled
         -Dlibbluray=enabled
